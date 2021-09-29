39088955eb62ac588a79e01680b9621e88732ff9
Studio Beesmart <studiobeesmart@gmail.com>
2021-09-29 16:59:42 +0700

    Ubah lagi Title

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>VAMoS - Signin</title>
+    <title>VAMoS - Sign In</title>
     <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 10]>
